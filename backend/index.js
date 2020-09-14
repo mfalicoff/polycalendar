c9a1d1b7063835436cafbdffff7cb3cb9a354919
Maximiliano <mfalicoff@yahoo.com>
2020-09-13 20:49:34 -0400

    console.log for normal classes

--- backend/index.js
+++ backend/index.js
@@ -53,12 +53,38 @@ app.get('/', async (req, res) => {
 						elem.children[1].children[1].children[3].children[3]
 							.children[7].children[0].data;
 				}
-				console.log(
-					CoursTableGroupe,
-					CoursTableJour,
-					CoursTableHeure,
-					CoursTableLocal
-				);
+				// console.log(
+				// 	CoursTableGroupe,
+				// 	CoursTableJour,
+				// 	CoursTableHeure,
+				// 	CoursTableLocal
+				// );
+			} else {
+
+
+				//pour les cours theoriques
+				let currentTableCoursTH = elem.children[1].children[1].children[3]
+				for (let j = 3; j < currentTableCoursTH.length; j = j + 2) {
+					let CoursTableGroupe = currentTableCoursTH.children[j].children[1].children[0].data;// nous donne groupe
+					let CoursTableJour = currentTableCoursTH.children[j].children[3].children[0].data; // nous donne le jours
+					let CoursTableHeure =currentTableCoursTH.children[j].children[5].children[0].data; // nous donne l'heure
+					let CoursTableLocal = currentTableCoursTH.children[j].children[7].children[0].data; //nous donne le local
+					console.log(i, CoursTableGroupe, CoursTableJour, CoursTableHeure, CoursTableLocal)
+
+				}
+				//traveaux pratiques
+				let currentTableCoursTP = elem.children[3].children[1].children[3]
+				for (let j = 3; j < currentTableCoursTP.children.length; j = j + 2) {
+					let CoursTableGroupe = currentTableCoursTP.children[j].children[1].children[0].data;// nous donne groupe
+					let CoursTableJour = currentTableCoursTP.children[j].children[3].children[0].data; // nous donne le jours
+					let CoursTableHeure = currentTableCoursTP.children[j].children[5].children[0].data; // nous donne l'heure
+					let CoursTableLocal = currentTableCoursTP.children[j].children[7].children[0].data; //nous donne le local
+					console.log(i, CoursTableGroupe, CoursTableJour, CoursTableHeure, CoursTableLocal);
+
+				}
+
+
+
 			}
 		});
 	//console.log(data)
