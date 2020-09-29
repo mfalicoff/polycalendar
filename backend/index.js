d5d71c50f1d7655fb6e2f3adb92881af02c5df18
Maximiliano <mfalicoff@yahoo.com>
2020-09-29 14:53:37 -0400

    class Storage

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,37 @@ const express = require('express');
 const polycrawler = require('./services/polyCrawler');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose')
+const Class= require('./models/class')
 
 app.use(cors());
 app.use(express.json());
 
-const password = process.argv.DB_TEST_PASS
-const url = `mongodb+srv://mazil007:${password}@cluster0.3xdqk.mongodb.net/<dbname>?retryWrites=true&w=majority`
+const password = process.env.DBpass
+const url = `mongodb+srv://mazil007:${password}@cluster0.3xdqk.mongodb.net/PolyCalendarTest?retryWrites=true&w=majority`
+console.log('connecting to', url)
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 
 app.post('/api/Admin/createSemester', async (req, res) => {
 	console.log(req.body);
-	let schedule = await polycrawler.polycrawler();
-	console.log(schedule);
+	let repertoireCours = await polycrawler.polycrawler();
+	repertoireCours.map(async (cours) => {
+		let coursDB = new Class({
+			name: cours.nom,
+			horraire: cours.horraire
+		})
+		let ok = await coursDB.save()
+		console.log(ok);
+	})
+	 
 	res.status(200).json({ status: 'semester created' });
 });
 
