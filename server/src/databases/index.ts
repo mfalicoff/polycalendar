d40344c49ef53f75f873fe8aecbbd1e9f709a84d
mazilious <mfalicoff@yahoo.com>
2022-02-07 19:02:41 -0500

    server template

---
+++ server/src/databases/index.ts
@@ -0,0 +1,10 @@
+import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+
+export const dbConnection = {
+  url: `${DB_HOST}/${DB_DATABASE}`,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+};
