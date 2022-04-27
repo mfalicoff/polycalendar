7af6848c5e571bd5310aa9f183f21dcfa23248c8
Maximiliano Falicoff <mfalicoff@yahoo.com>
2022-04-26 20:43:53 -0400

    fixing env file

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config({ path: require('find-config')('.env') });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
