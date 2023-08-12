68cde4bbacb4c375f5d42358d4f9d31b4b23e914
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-08-12 10:26:33 -0400

    disabled routes by passing authmiddleware

--- server/src/routes/class.route.ts
+++ server/src/routes/class.route.ts
@@ -20,14 +20,19 @@ class ClassRoute implements Routes {
     this.router.post(`${this.path}/manyName`, this.classController.getManyClassByName);
     this.router.get(`${this.path}/many`, this.classController.getManyClass);
     this.router.get(`${this.path}/:id`, this.classController.getClassById);
-    this.router.post(`${this.path}`, validationMiddleware(CreateClassDto, 'body'), this.classController.createClass);
+    this.router.post(
+      `${this.path}`,
+      authMiddleware,
+      validationMiddleware(CreateClassDto, 'body'),
+      this.classController.createClass,
+    );
     this.router.post(`${this.path}/scrape`, authMiddleware, this.classController.scrapeClasses);
     this.router.put(
       `${this.path}/:id`,
       validationMiddleware(CreateClassDto, 'body', true),
       this.classController.updateClass,
     );
-    this.router.delete(`${this.path}/:id`, this.classController.deleteClass);
+    this.router.delete(`${this.path}/:id`, authMiddleware, this.classController.deleteClass);
   }
 }
 
